75fd84ada0c8a36e57985fadafc10beb02c9c950
herdialfachri <alfachri75@gmail.com>
2026-05-11 21:55:43 +0700

    Displaying complaint and submission user

--- routes/web.php
+++ routes/web.php
@@ -1,27 +1,37 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
+use App\Models\Complaint;
+use App\Models\Submission;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\SubmissionController;
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ComplaintController;
+use App\Http\Controllers\SubmissionController;
 
 Route::get('/', function () {
     return view('index');
 })->name('home');
 
-// User
 Route::middleware(['auth', 'verified', 'role:user'])
     ->prefix('dashboard')
     ->name('dashboard.')
     ->group(function () {
 
         Route::get('/', function () {
-            return view('user.dashboard');
-        })->name('index');
 
-        // ======================
-        // Pengajuan
-        // ======================
+            $complaints = Complaint::where('user_id', Auth::id())
+                ->latest()
+                ->get();
+
+            $submissions = Submission::where('user_id', Auth::id())
+                ->latest()
+                ->get();
+
+            return view('user.dashboard', compact(
+                'complaints',
+                'submissions'
+            ));
+        })->name('index');
 
         Route::get('/submissions', [SubmissionController::class, 'index'])
             ->name('submissions');
@@ -32,10 +42,6 @@ Route::middleware(['auth', 'verified', 'role:user'])
         Route::post('/submissions', [SubmissionController::class, 'store'])
             ->name('submissions.store');
 
-        // ======================
-        // Pengaduan
-        // ======================
-
         Route::get('/complaints', [ComplaintController::class, 'index'])
             ->name('complaints');
 
@@ -45,39 +51,41 @@ Route::middleware(['auth', 'verified', 'role:user'])
         Route::post('/complaints', [ComplaintController::class, 'store'])
             ->name('complaints.store');
 
-        // ======================
-        // Profile
-        // ======================
-
         Route::get('/profile', function () {
             return view('user.profile');
         })->name('profile');
     });
 
-// Admin
 Route::middleware(['auth', 'verified', 'role:admin'])
     ->prefix('dashboard/admin')
     ->name('admin.')
     ->group(function () {
+
         Route::get('/', function () {
             return view('admin.dashboard');
         })->name('dashboard');
     });
 
-// Staff
 Route::middleware(['auth', 'verified', 'role:staff'])
     ->prefix('dashboard/staff')
     ->name('staff.')
     ->group(function () {
+
         Route::get('/', function () {
             return view('staff.dashboard');
         })->name('dashboard');
     });
 
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/profile', [ProfileController::class, 'edit'])
+        ->name('profile.edit');
+
+    Route::patch('/profile', [ProfileController::class, 'update'])
+        ->name('profile.update');
+
+    Route::delete('/profile', [ProfileController::class, 'destroy'])
+        ->name('profile.destroy');
 });
 
 require __DIR__ . '/auth.php';
